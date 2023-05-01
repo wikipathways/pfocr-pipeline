e14008f9e51aa3307f1b7e51d4f0d78ad7743ef6
Yihang Xin <yihang.xin@gladstone.ucsf.edu>
2023-05-01 01:30:52 -0700

    update step_1

--- 2_gcv_automl/step_1.sh
+++ 2_gcv_automl/step_1.sh
@@ -6,6 +6,3 @@
 
 # Upload images to the google cloud storage, replace [Bucket Name] with your Bucket Name
 gsutil -m cp -r automl_train_15k_20200224 gs://[Bucket Name]
-
-# Download all gsutil URI from Bucket as csv file
-gsutil ls -r gs://[Bucket Name] > gsutil_url.csv

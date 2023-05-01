391f04c81f63c21334590f3fef342cca94914628
Yihang Xin <yihang.xin@gladstone.ucsf.edu>
2023-05-01 00:47:53 -0700

    add shell script

--- 2_gcv_automl/step_1.sh
+++ 2_gcv_automl/step_1.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-# First download automl_train_15k_20200224 from Dropbox
+# First download automl_train_15k_20200224 to your local machine from Dropbox
 
 # Install gsutil from https://cloud.google.com/storage/docs/gsutil_install
 
-# Upload images to the goold cloud storage, replace [Bucket Name] with your Bucket Name
+# Upload images to the google cloud storage, replace [Bucket Name] with your Bucket Name
 gsutil -m cp -r automl_train_15k_20200224 gs://[Bucket Name]
 
-# Download all gsutil URI from Bucket
+# Download all gsutil URI from Bucket as csv file
 gsutil ls -r gs://[Bucket Name] > gsutil_url.csv

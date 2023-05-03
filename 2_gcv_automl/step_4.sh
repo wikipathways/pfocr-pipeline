ef0455f5e1cc0d5d2b5bdd69595ad29fec7445ac
Yihang Xin <yihang.xin@gladstone.ucsf.edu>
2023-05-03 00:41:03 -0700

    add step_4

---
+++ 2_gcv_automl/step_4.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# Train the model
+curl -X POST \
+    -H "Authorization: Bearer $(gcloud auth print-access-token)" \
+    -H "Content-Type: application/json; charset=utf-8" \
+    -d @request.json \
+    "https://us-central1-aiplatform.googleapis.com/v1/projects/pfocr-384122/locations/us-central1/trainingPipelines"

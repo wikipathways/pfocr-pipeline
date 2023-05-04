a487d7b9786693f8d9049ef09541ded5fde321c8
Yihang Xin <46608109+yihangx@users.noreply.github.com>
2023-05-04 10:43:37 -0700

    Update step_1.sh

--- 2_gcv_automl/step_1.sh
+++ 2_gcv_automl/step_1.sh
@@ -6,5 +6,7 @@
 
 # Install gsutil from https://cloud.google.com/storage/docs/gsutil_install
 
+# Create a bucket. Keep bucket name secret. Caution: delete bucket after training.
+
 # Upload images to the google cloud storage, replace [Bucket Name] with your Bucket Name
 gsutil -m cp -r automl_train_15k_20200224 gs://[Bucket Name]

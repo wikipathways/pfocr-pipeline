e788b9453171ef542a56d0ed0a2e0bb8e60a1163
Yihang Xin <yihang.xin@gladstone.ucsf.edu>
2023-05-08 23:32:27 -0700

    add prediction

---
+++ 2_gcv_automl/train_step_3.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# Import Data
+# chmod +x step_3.sh
+# Upload import_files.csv to bucket via shell or GUI first
+# https://cloud.google.com/vertex-ai/docs/image-data/classification/create-dataset#aiplatform_import_data_image_classification_single_label_sample-drest
+
+# Create dataset and replace Bucket ID
+curl -X POST \
+    -H "Authorization: Bearer $(gcloud auth print-access-token)" \
+    -H "Content-Type: application/json; charset=utf-8" \
+    -d @train_request_import.json \
+    "https://us-central1-aiplatform.googleapis.com/v1/projects/pfocr-384122/locations/us-central1/datasets/[Bucket ID]:import"

87aeba8b92bb0b9c9a5ddf4bc194049401a8c909
Yihang Xin <46608109+yihangx@users.noreply.github.com>
2023-05-16 15:39:55 -0700

    Update predict_step_6.sh

--- 2_gcv_automl/predict_step_6.sh
+++ 2_gcv_automl/predict_step_6.sh
@@ -1,5 +1,6 @@
 # When the prediction is done, the result will be stored in the bucket.
 # Download all jsonl files from predictions_00001.jsonl to predcitions_00010 to your machine.
+# We can easily concat these jsonl files to a single jsonl file and then run process_raw_rds.R to process the data.
 # Then we can delete all data in the bucket:
 
 gsutil -m rm -r gs://your-bucket-name/*
